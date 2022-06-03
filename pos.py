3d3fc31c7e7233698b62164e4e6b3213e49fecd9
Guga's PC <smiron1703@gmail.com>
2022-06-03 15:22:43 +0500

    started making ui for pos system

---
+++ pos.py
@@ -0,0 +1,38 @@
+import sys
+from datetime import datetime
+from PyQt5 import uic, QtGui
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLCDNumber, QHeaderView
+from PyQt5.QtCore import QTimer
+
+
+class UI(QMainWindow):
+    def __init__(self):
+        super(UI, self).__init__()
+
+        # Load the ui file
+        uic.loadUi("pos.ui", self)
+        self.lcd = self.findChild(QLCDNumber, 'lcdNumber')
+        self.lcd.setDigitCount(8)
+        self.lcd.setSegmentStyle(QLCDNumber.Flat)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.lcd_number)
+
+        # Start the timer and update every second
+        self.timer.start(1000)
+
+        # Call the lcd function
+        self.lcd_number()
+
+        # Show the app
+        self.show()
+    
+    def lcd_number(self):
+        time = datetime.now()
+        f_time = time.strftime("%H:%M:%S")
+
+        self.lcd.display(f_time)
+
+
+app = QApplication(sys.argv)
+UIWindow = UI()
+app.exec_()
